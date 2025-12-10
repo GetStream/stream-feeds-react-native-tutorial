469f3c713f59b408890e2acf8ad1a5ea18986b3e
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-12-10 11:46:05 +0100

    Connect to the Stream API

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,50 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import {
+  StreamFeeds,
+  useCreateFeedsClient,
+} from "@stream-io/feeds-react-native-sdk";
+import { API_KEY, CURRENT_USER } from "@/user";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const client = useCreateFeedsClient({
+    apiKey: API_KEY,
+    tokenOrProvider: CURRENT_USER.token,
+    userData: {
+      id: CURRENT_USER.id,
+      name: CURRENT_USER.name,
+    },
+  });
+
+  if (!client) {
+    return null;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="comments-modal" options={{ presentation: 'modal', title: 'Comments' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <StreamFeeds client={client}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="comments-modal"
+            options={{ presentation: "modal", title: "Comments" }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </StreamFeeds>
   );
 }
