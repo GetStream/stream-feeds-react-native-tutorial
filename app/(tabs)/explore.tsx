013d3a9b32b47380885f157e6f6a85b6abdc4761
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-12-10 19:35:03 +0100

    Explore page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,3 +1,34 @@
-export default function TabTwoScreen() {
-  return null;
-}
+import {
+  useFeedsClient,
+  useClientConnectedUser,
+  StreamFeed,
+} from "@stream-io/feeds-react-native-sdk";
+import { useEffect, useMemo } from "react";
+import { ActivityList } from "@/components/activity/ActivityList";
+
+export default () => {
+  const client = useFeedsClient();
+  const currentUser = useClientConnectedUser();
+  const feed = useMemo(() => {
+    if (!currentUser?.id || !client) {
+      return undefined;
+    }
+    return client.feed("foryou", currentUser.id);
+  }, [client, currentUser?.id]);
+
+  useEffect(() => {
+    if (feed) {
+      feed.getOrCreate({ limit: 10 });
+    }
+  }, [feed]);
+
+  if (!feed) {
+    return null;
+  }
+
+  return (
+    <StreamFeed feed={feed}>
+      <ActivityList />
+    </StreamFeed>
+  );
+};
